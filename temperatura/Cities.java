da53476d2cd8e8ae89eaa3d63dca48a5c9b21cd5
jleva <Sdisjkdsakld@ajdshfkjasdhf>
2022-09-22 22:04:06 -0300

    First commit

---
+++ temperatura/Cities.java
@@ -0,0 +1,15 @@
+package temperatura;
+
+public class Cities {
+    public int number;
+    public String name;
+    public int minimum;
+    public int maximum;
+
+    public Cities(int num, String nam, int min, int max) {
+        number = num;
+        name = nam;
+        minimum = min;
+        maximum = max;
+    }
+}
